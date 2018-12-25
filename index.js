1909edf427bb52586e19b93f5f38c43eecd57e04
GulfamAnsari1994 <gulfam.ansari@elucidata.io>
2018-12-25 18:26:47 +0530

    connect my sql

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const PORT = 8080;
-var http = require("http");  
-http.createServer(function (request, response) {  
- // Send the HTTP header   
-   // HTTP Status: 200 : OK  
-   // Content Type: text/plain  
-   response.writeHead(200, {'Content-Type': 'text/plain'});  
-   // Send the response body as "Hello World"  
-   response.end('Hello World\n');  
-}).listen(PORT);  
-// Console will print the message  
-console.log(`Listening on ${ PORT }`);
+var express = require('express');
+var mysql = require('mysql');
+
+var app = express();
+var port = 4200;
+var connection = mysql.createConnection({
+    host     : '166.62.6.78',
+    user     : 'gulfamansari1515',
+    password : 'Gulfam@45683968',
+    database: 'DroidTechKnow'
+});
+app.use(express.static('public'));
+
+
+app.get('/', (req, res)=>{
+    getAllArticleList().then((data)=>{
+        res.send(data);
+    });
+})
+
+function getAllArticleList() {
+    var promise = new Promise((resolve, reject)=>{
+        connection.connect();
+        connection.query('SELECT * from article', (error, results, fields)=> {
+            if (error) reject(error);
+            resolve(results);
+        });
+    });
+    connection.end();
+    return promise;
+}
+
+app.listen(port, ()=>{
+    console.log('The server is running on port: ' + port);
+})
