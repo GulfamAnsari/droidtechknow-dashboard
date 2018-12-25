5c6d611930befc441dbfd35e384e8dadeb37f0c1
GulfamAnsari1994 <gulfam.ansari@elucidata.io>
2018-12-25 18:35:24 +0530

    update port

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var express = require('express');
 var mysql = require('mysql');
 
 var app = express();
-var port = 4200;
+const path = require('path')
+const PORT = process.env.PORT || 5000;
 var connection = mysql.createConnection({
     host     : '166.62.6.78',
     user     : 'gulfamansari1515',
     password : 'Gulfam@45683968',
     database: 'DroidTechKnow'
 });
-app.use(express.static('public'));
 
-
+app.use(express.static(path.join(__dirname, 'public')));
+  
 app.get('/', (req, res)=>{
     getAllArticleList().then((data)=>{
         res.send(data);
@@ -30,6 +31,4 @@ function getAllArticleList() {
     return promise;
 }
 
-app.listen(port, ()=>{
-    console.log('The server is running on port: ' + port);
-})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
