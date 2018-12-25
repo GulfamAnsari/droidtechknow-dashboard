c1913c69f63eedc9879c8f75ef8679c8852dc85f
GulfamAnsari1994 <gulfam.ansari@elucidata.io>
2018-12-25 23:46:30 +0530

    change db to local

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000;
 var connection = mysql.createConnection({
-    host     : '166.62.6.78',
-    user     : 'gulfamansari1515',
-    password : 'Gulfam@45683968',
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
     database: 'DroidTechKnow'
 });
 
@@ -15,23 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')));
   
 app.get('/', (req, res)=>{
     getAllArticleList().then((data)=>{
-        var html = `<table><thead>
-        <tr>
-          <th>Post</th>
-          <th>Title</th>
-          <th>Author</th>
-        </tr>
-      </thead>
-      <tbody>`;
-        for(var i=0; i< data.length; i++) {
-            var articleTitle = data[i]['articleTitle'];
-            var post = data[i]['post'];
-            var author = data[i]['author'];
-            html = html.concat(`<tr><td>${post}</td><td>${articleTitle}</td><td>${author}</td></tr>`)
-        }
-        html = html.concat("</tbody></table>")
-        res.send(html);
+        res.send(data);
     });
+    // res.sendFile(path.join(__dirname + '/public/static/index.html'));
 })
 
 function getAllArticleList() {
