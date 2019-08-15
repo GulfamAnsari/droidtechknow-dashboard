32401ec66e5348ae9313a142a3c9aa43c94559a7
gulfamansari <gulfamansari1515@gmail.com>
2019-08-15 16:02:35 +0530

    add cred file

---
+++ backend/cred.js
@@ -0,0 +1,8 @@
+
+
+// module.exports = DATABASE_DROID_CRED_OBJECT = {
+//   host: process.env.DROID_HOST,
+//   user: process.env.DROID_USERNAME,
+//   password: process.env.DROID_PASSWORD,
+//   database: process.env.DROID_DATABASE
+// }
