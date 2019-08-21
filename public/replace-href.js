35fdd00d8e67745fa99587cdefce08a602d80190
gulfamansari <gulfamansari1515@gmail.com>
2019-08-21 17:13:06 +0530

    add href for react files

---
+++ public/replace-href.js
@@ -0,0 +1,14 @@
+// This will replace the href in the html file
+var fs = require('fs')
+var path = require('path');
+var file = path.join(__dirname + '../../public/react/index.html');
+fs.readFile(file, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = data.replace(/static/g, 'react/static');
+
+  fs.writeFile(file, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
