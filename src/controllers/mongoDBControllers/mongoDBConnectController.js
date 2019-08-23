ec077c00e1d94296e5a5067c8bc90c92e150f57c
gulfamansari <gulfamansari1515@gmail.com>
2019-08-23 20:57:56 +0530

    update mongodb version

--- src/controllers/mongoDBControllers/mongoDBConnectController.js
+++ src/controllers/mongoDBControllers/mongoDBConnectController.js
@@ -5,10 +5,17 @@ class MongoDBConnectController {
   constructor() { }
 
   connectMongoDB() {
+    var url = "mongodb://localhost:27017/droid";
+    if (CRED_OBJECTS.MONGO.password) {
+      // need to double encode username and password
+      const password = encodeURIComponent(encodeURIComponent(CRED_OBJECTS.MONGO.password));
+      url = 'mongodb+srv://' + CRED_OBJECTS.MONGO.user + ':' + password + CRED_OBJECTS.MONGO.host;
+    }
+
     // var url = "mongodb://localhost:27017/droid";
     // need to double encode username and password
-    const password = encodeURIComponent(encodeURIComponent(CRED_OBJECTS.MONGO.password));
-    var url = 'mongodb+srv://' + CRED_OBJECTS.MONGO.user + ':' + password + CRED_OBJECTS.MONGO.host;
+    // const password = encodeURIComponent(encodeURIComponent(CRED_OBJECTS.MONGO.password));
+    // var url = 'mongodb+srv://' + CRED_OBJECTS.MONGO.user + ':' + password + CRED_OBJECTS.MONGO.host;
 
     return new Promise((resolve, reject) => {
       MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
