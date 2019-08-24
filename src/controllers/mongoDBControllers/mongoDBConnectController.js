ad463f96ec739a696e62c988917c0433570bedfb
gulfamansari <gulfamansari1515@gmail.com>
2019-08-24 16:22:59 +0530

    update url

--- src/controllers/mongoDBControllers/mongoDBConnectController.js
+++ src/controllers/mongoDBControllers/mongoDBConnectController.js
@@ -9,9 +9,10 @@ class MongoDBConnectController {
     if (CRED_OBJECTS.MONGO.password) {
       // need to double encode username and password
       const password = encodeURIComponent(encodeURIComponent(CRED_OBJECTS.MONGO.password));
-      url = 'mongodb+srv://' + CRED_OBJECTS.MONGO.user + ':' + password + CRED_OBJECTS.MONGO.host;
-    }
+      // url = 'mongodb+srv://' + CRED_OBJECTS.MONGO.user + ':' + password + CRED_OBJECTS.MONGO.host;
+      url = `mongodb://${CRED_OBJECTS.MONGO.user}:${encodeURIComponent(CRED_OBJECTS.MONGO.password)}@cluster0-shard-00-00-pvtht.mongodb.net:27017,cluster0-shard-00-01-pvtht.mongodb.net:27017,cluster0-shard-00-02-pvtht.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
+    }
     // var url = "mongodb://localhost:27017/droid";
     // need to double encode username and password
     // const password = encodeURIComponent(encodeURIComponent(CRED_OBJECTS.MONGO.password));
