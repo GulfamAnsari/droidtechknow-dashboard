ac8111045b9f4e39f28a7ab6e60f08464cf07aac
gulfamansari <gulfamansari1515@gmail.com>
2019-08-25 15:53:35 +0530

    add interceptor

--- frontend/react/src/helper/backend.js
+++ frontend/react/src/helper/backend.js
@@ -1,4 +1,7 @@
 import Axios from 'axios';
+import * as HELPER from './helper-functions';
+
+const excludeUrlForAuthenticate = ['/login', '/signup', 'http://ip-api.com/json'];
 
 const AxiosInstance = Axios.create({
   headers: { 'Content-Type': 'application/json' }
@@ -10,7 +13,15 @@ if (window.location.hostname === 'localhost') {
   AxiosInstance.defaults.baseURL = 'http://droidtechknow-dashboard.herokuapp.com';
 }
 
-// instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// Request interceptor
+AxiosInstance.interceptors.request.use((config) => {
+  if (!excludeUrlForAuthenticate.includes(config.url)) {
+    AxiosInstance.defaults.headers.common['token'] = HELPER.getCookie('token');
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 export const get = (url, options = null) => AxiosInstance.get(url, options);
 export const post = (url, data, options = null) => AxiosInstance.post(url, data, options);
