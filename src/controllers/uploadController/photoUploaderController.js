1dcdc8b60c2bf7bc7300ce6ecafbde959bfe93b8
gulfamansari <gulfamansari1515@gmail.com>
2019-09-10 20:31:50 +0530

    complete upload function

--- src/controllers/uploadController/photoUploaderController.js
+++ src/controllers/uploadController/photoUploaderController.js
@@ -18,14 +18,15 @@ class PhotoUploaderController {
   uploadAndSave(req, res, db) {
     return new Promise((resolve, reject) => {
       const options = { unique_filename: false, overwrite: true, use_filename: true };
-      const file = req.body.payload.file;
-      cloudinary.uploader.upload(file, options, (error, result) => {
-        if (error) {
-          reject(error);
-        }
+      const filePath = req.body.payload.userImage;
+
+      cloudinary.uploader.upload(filePath, options, (error, result) => {
+        if (error) reject(error);
+        
         const email = helperController.decoreJWT(req.headers.token).email;
         const imageUrl = result.secure_url;
         var dbo = db.db(CRED_OBJECTS.database);
+
         dbo.collection(COLLECTIONS.LOGIN).find({ email }).toArray((err, dbResult) => {
           if (err) reject(err);
           var newvalues = { $set: { profile_image: imageUrl } };
