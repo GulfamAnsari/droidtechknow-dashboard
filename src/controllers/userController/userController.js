35513d67dd0fb6eafdb61d993204d3170ea4cf71
gulfamansari <gulfamansari1515@gmail.com>
2019-09-12 19:56:33 +0530

    add image url

--- src/controllers/userController/userController.js
+++ src/controllers/userController/userController.js
@@ -18,6 +18,7 @@ class UserController {
   updateInformation(req, res, db) {
     return new Promise((resolve, reject) => {
       const filePath = req.body.payload.userImage;
+      const { payload } = req.body;
       const email = helperController.decoreJWT(req.headers.token).email;
       // const email = "gulfam@gmail.com"
       const options = {
@@ -29,10 +30,11 @@ class UserController {
       };
       cloudinary.uploader.upload(filePath, options, (error, result) => {
         if (error) reject(error);
+        payload.userImage = result.secure_url
         var dbo = db.db(CRED_OBJECTS.database);
         dbo.collection(COLLECTIONS.LOGIN).find({ email }).toArray((err, dbResult) => {
           if (err) reject(err);
-          var newvalues = { $set: { ...req.body.payload } };
+          var newvalues = { $set: payload };
           mongoDBConnectController.updateOne(db, COLLECTIONS.LOGIN, { email }, newvalues).then((res) => {
             resolve(newvalues);
           }, err => reject(err));
