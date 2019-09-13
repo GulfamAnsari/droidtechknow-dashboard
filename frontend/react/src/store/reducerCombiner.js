d040e6c755392798d50bbdf0459a20c74593c86e
gulfamansari <gulfamansari1515@gmail.com>
2019-09-13 22:31:43 +0530

    reset all reducer values of logout

--- frontend/react/src/store/reducerCombiner.js
+++ frontend/react/src/store/reducerCombiner.js
@@ -5,10 +5,18 @@ import Main_Reducer from './reducer';
 import Profile_Reducer from '../pages/profile/store/reducer'
 import Todo_Reducer from '../pages/todo/store/reducer'
 
-const Reducer = combineReducers({
+const rootReducer = combineReducers({
   Main_Reducer,
   Todo_Reducer,
   Profile_Reducer
 })
+
+// export default Reducer;
+const Reducer = (state, action) => {
+  if (action.type === 'LOG_OUT') {
+    state = undefined;
+  }
+  return rootReducer(state, action);
+};
 
 export default Reducer;
