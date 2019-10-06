72e84fb6edb234d7b43b4eadb8260a49d291b023
Gulfam Ansari <gulfamansari@197NODMB24235.local>
2019-10-06 20:16:38 +0530

    update bundle file

---
+++ public/react/precache-manifest.3dfe3868df672d8adeb53c39bd7b0b36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2a702b5d3d635b80b140d07115ca62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8650058ae44c5d148e79",
+    "url": "/static/css/main.4cdca73c.chunk.css"
+  },
+  {
+    "revision": "49e4ca1fe29076449c2c",
+    "url": "/static/js/2.ee7c1152.chunk.js"
+  },
+  {
+    "revision": "8650058ae44c5d148e79",
+    "url": "/static/js/main.a47bdbae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "360a359b3b4fd5c8127276d8eea54801",
+    "url": "/static/media/india.360a359b.jpg"
+  },
+  {
+    "revision": "ffd84e9728194750ef527d796ad80cc1",
+    "url": "/static/media/login-bg.ffd84e97.jpg"
+  },
+  {
+    "revision": "cb55b0d6892d15e9adad08bfd9b9c9cd",
+    "url": "/static/media/sidebar-1.cb55b0d6.jpg"
+  }
+]);
