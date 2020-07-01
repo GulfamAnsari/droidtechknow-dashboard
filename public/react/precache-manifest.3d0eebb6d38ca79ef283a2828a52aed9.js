3ffba5f2f4c67b73b2fdccd3c9ca5f95e2efefd1
gulfamansari <gulfamansari1515@gmail.com>
2020-07-02 02:16:59 +0530

    update bundle file

---
+++ public/react/precache-manifest.3d0eebb6d38ca79ef283a2828a52aed9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14c9110ea540335cedbb65396b78c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c2e7f158cdc31570ffd",
+    "url": "/static/css/main.67ed20a7.chunk.css"
+  },
+  {
+    "revision": "78f44aba959368fecf8a",
+    "url": "/static/js/2.824e71d0.chunk.js"
+  },
+  {
+    "revision": "8c2e7f158cdc31570ffd",
+    "url": "/static/js/main.1ddbae1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04b716c08f25ec0e66d6cb07bccbb787",
+    "url": "/static/media/1.04b716c0.jpg"
+  },
+  {
+    "revision": "ffd84e9728194750ef527d796ad80cc1",
+    "url": "/static/media/login-bg.ffd84e97.jpg"
+  },
+  {
+    "revision": "cb55b0d6892d15e9adad08bfd9b9c9cd",
+    "url": "/static/media/sidebar-1.cb55b0d6.jpg"
+  }
+]);
