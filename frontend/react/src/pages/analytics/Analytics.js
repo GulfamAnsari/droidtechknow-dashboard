eb3a180e583f7779bcb4bed2c0205970bca7341f
gulfamansari <gulfamansari1515@gmail.com>
2020-07-02 12:38:05 +0530

    update

--- frontend/react/src/pages/analytics/Analytics.js
+++ frontend/react/src/pages/analytics/Analytics.js
@@ -4,6 +4,7 @@ import { LOCATION_API_URL } from '../../constants';
 import './Analytics.scss';
 import * as Notiflix from '../../helper/notiflix';
 import Table from '../../components/Table/Table';
+import SmartDataTable from 'react-smart-data-table';
 
 export default class Analytics extends Component {
     constructor(props) {
@@ -14,10 +15,19 @@ export default class Analytics extends Component {
                 status: false,
                 msg: ''
             },
+            date: this.convertDate(String(new Date())),
+            pageSize: 10,
             appData: null,
             table: null
         }
     }
+
+    convertDate = (d) => {
+        var parts = d.split(" ");
+        var months = { Jan: "01", Feb: "02", Mar: "03", Apr: "04", May: "05", Jun: "06", Jul: "07", Aug: "08", Sep: "09", Oct: "10", Nov: "11", Dec: "12" };
+        return parts[2] + "-" + months[parts[1]] + "-" + parts[3];
+    }
+
 
     getTableData = (date) => {
         const { appData } = this.state;
@@ -51,20 +61,41 @@ export default class Analytics extends Component {
         }
     }
 
-    render() {
-        const { loading, error, appData, table } = this.state;
-        var date = '02-07-2020';
+    select = ()=> {
+        var e = document.getElementById("paginagion");
+        var value = e.options[e.selectedIndex].value;
+        this.setState({pageSize: value})
+    }
 
+
+    render() {
+        const { loading, error, appData, table, date, pageSize } = this.state;
         return (
             <React.Fragment>
                 {loading ? Notiflix.loading('Loading Analytics. Please wait...') : Notiflix.remove()}
                 {error.status && !loading ? Notiflix.notify('Failure', error.msg) : ''}
                 <div className="content">
                     <section className="main __Analytics">
-                        <p><stong>Date: {date}</stong></p>
+                        <span><stong>Date: {date}</stong></span>
                         <input type="text" id="inputDate" />
                         <button type="submit" onClick={() => { this.chnageDate() }} >Change</button>
-                        {appData ? <Table tableData={{ title: 'Droidtechkow Analytics', description: 'See your analytics here', data: table ? table : this.getTableData(date) }} /> : null}
+                        <select id="paginagion" onChange={()=>{this.select()}}>
+                            <option>10</option>
+                            <option>20</option>
+                            <option>30</option>
+                            <option>50</option>
+                            <option>100</option>
+                            <option>500</option>
+                            <option>1000</option>
+                        </select>
+
+                        {appData ? <SmartDataTable
+                            data={table ? table : this.getTableData(date)}
+                            name='Droidtechknow analytics table'
+                            className='ui compact selectable table'
+                            sortable
+                            perPage={pageSize}
+                        /> : null}
                     </section>
                 </div>
             </React.Fragment>
